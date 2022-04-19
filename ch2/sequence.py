0edcf16064cf33881f9e89c5edc2229f8d1c468b
Jeong Yo Han <ingyeoking13@gmail.com>
2022-04-20 02:02:44 +0900

    feat : ch 2.3.4
    - saves missing

--- ch2/sequence.py
+++ ch2/sequence.py
@@ -24,30 +24,3 @@ print(arr_ge, arr_ge[0], arr_ge[1])
 tshirts = ('%s %s' % (c, s) for c in colors for s in sizes) # generator expression
 print([t for t in tshirts])
 
-# tuple unpacking
-lax_coordinates = (33.9425, -118.408056)
-latitude, longitude = lax_coordinates 
-print(latitude, longitude)
-
-latitude, longitude = longitude, latitude # tuple unpacking - exchange value -_- ;;;!!!
-print(latitude, longitude)
-
-print( divmod(20, 8) ) # tuple unpacking with asterisk
-t = (20, 8) 
-print(divmod(*t)) # 
-quotient, remainder = divmod(*t)
-print(quotient, remainder) 
-
-a,b, *rest = range(5) # asterisk for exceed items
-print(a, b, rest)
-a, b, *rest = range(3)
-print(a, b, rest)
-a, b, *rest = range(2)
-print(a, b, rest)
-a, *rest, b = range(5)
-print(a, b, rest)
-*head, a, b = range(5)
-print(head, a, b)
-
-
-
